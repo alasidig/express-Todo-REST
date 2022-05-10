8e99159080aa86adcad95ebee6fd92a0b2397b93
alasidig <ala.sidig@gmail.com>
2022-05-10 08:17:03 +0300

    Enable CORS

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 app.use(express.json())
+app.use(cors())
 const {getTasks, addTask, getTaskById, updateTask, deleteTask, getObjectOr404} = require("./models/tasksDbController");
 // const {getTasks, addTask, getTaskById, updateTask, deleteTask, getObjectOr404} = require("./models/tasksController");
 
